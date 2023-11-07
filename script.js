54e3cd84ecec4347f4bdd2bb6f5596e0ad1ec0ee
Moon Chu <mmoonchu2@gmail.com>
2023-11-07 10:13:47 -0500

    Wrote wireframe.txt (essentially pseudocode)

---
+++ script.js
@@ -0,0 +1,11 @@
+// make character constructor (name, movespeed, health, damage)
+    // children classes:    
+    // players, projectiles
+    // potentially creatures later on
+// make 2 players
+    // add:
+        // properties: weapon
+        // methods: controls
+            // while (wasd) {translateX/Y}
+
+
